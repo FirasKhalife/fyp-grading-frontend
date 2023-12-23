1d6dcaecc0542ce9c4b1c9f1546a39cc00abcb2c
Gaelle327 <gaellesaid65@gmail.com>
2023-12-23 12:24:37 +0200

    setting up the frontend pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Hello, world!/i);
-    expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+    test('renders learn react link', async () => {
+        await act(async () => {
+            render(<App />);
+        });
+
+        const linkElement = screen.getByText(/learn react/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
